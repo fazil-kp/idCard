f8c36dc4b866f823ce9dc725ac1e6177dfbfd231
Fazil <mfazilkp7@gmail.com>
2023-04-20 21:00:29 +0530

    ID card project

---
+++ android/app/src/main/kotlin/com/example/idcard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.idcard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: idCrad() ,
+      ),
+  );
+}
+
+class idCrad extends StatelessWidget {
+  const idCrad({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text("MFZ ID Card"),
+        centerTitle:true,
+        backgroundColor: Colors.grey[800],
+        elevation: 0.0,
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('images/profile.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 80.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+                'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Fazil',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'CURRENT MFZ LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '10',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(
+                    width: 8.0,
+                ),
+                Text(
+                    "mfazilkp10aa@gmail.com",
+                        style: TextStyle(
+                          color: Colors.grey[400],
+                          letterSpacing: 1.0,
+                          fontSize: 18.0,
+                        ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
